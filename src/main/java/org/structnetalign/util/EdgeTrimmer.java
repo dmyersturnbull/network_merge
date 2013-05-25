32f4f694f1298ea6218ecdac70c3ac46980d6691
dmyersturnbull <dmyersturnbull@gmail.com>
2013-05-24 23:45:18 -0700

    ConcurrentModificationException fix.

--- src/main/java/org/structnetalign/util/EdgeTrimmer.java
+++ src/main/java/org/structnetalign/util/EdgeTrimmer.java
@@ -9,43 +9,68 @@
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
+ * 
  * @author dmyersturnbull
  */
 package org.structnetalign.util;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.List;
+
+import org.structnetalign.Edge;
 
 import edu.uci.ics.jung.graph.Graph;
 
 /**
  * A simple graph transformation to remove edges with weights above or below a threshold.
+ * 
  * @author dmyersturnbull
- *
+ * 
  * @param <V>
  * @param <E>
  */
-public class EdgeTrimmer<V,E> {
+public class EdgeTrimmer<V, E> {
 
 	private final EdgeWeighter<E> edgeWeighter;
-	
+
+	public static <E extends Edge> EdgeTrimmer<Integer, E> forWeighted() {
+		EdgeWeighter<E> weighter = new EdgeWeighter<E>() {
+			@Override
+			public double getWeight(E e) {
+				return e.getWeight();
+			}
+		};
+		return new EdgeTrimmer<>(weighter);
+	}
+
 	public EdgeTrimmer(EdgeWeighter<E> edgeWeighter) {
 		super();
 		this.edgeWeighter = edgeWeighter;
 	}
 
-	public void trim(Graph<V,E> graph, double minimum) {
+	/**
+	 * Removes every edge with weight strictly less than {@code minimum}.
+	 */
+	public void trim(Graph<V, E> graph, double minimum) {
 		trim(graph, minimum, Double.MAX_VALUE);
 	}
-	
-	public void trim(Graph<V,E> graph, double minimum, double maximum) {
+
+	/**
+	 * Removes every edge with weight strictly less than {@code minimum} or strictly greater than {@code maximum}.
+	 */
+	public void trim(Graph<V, E> graph, double minimum, double maximum) {
 		Collection<E> edges = graph.getEdges();
 		Iterator<E> iter = edges.iterator();
+		List<E> edgesToRemove = new ArrayList<>();
 		while (iter.hasNext()) {
 			E edge = iter.next();
 			double weight = edgeWeighter.getWeight(edge);
-			if (weight < minimum || weight > maximum) graph.removeEdge(edge);
+			if (weight < minimum || weight > maximum) edgesToRemove.add(edge);
 		}
+		for (E edge : edgesToRemove)
+			graph.removeEdge(edge);
 	}
-	
+
 }
