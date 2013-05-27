b45308add96e3f2a1746309eb61d3acec5e1d54c
dmyersturnbull <dmyersturnbull@gmail.com>
2013-05-26 23:44:04 -0700

    Some critical bug fixes to HomologySearchJob.

--- src/main/java/org/structnetalign/cross/HomologySearchJob.java
+++ src/main/java/org/structnetalign/cross/HomologySearchJob.java
@@ -15,6 +15,8 @@
 
 package org.structnetalign.cross;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.Collection;
 import java.util.Deque;
 import java.util.HashMap;
@@ -26,14 +28,20 @@ import java.util.Set;
 import java.util.TreeMap;
 import java.util.concurrent.Callable;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.structnetalign.CleverGraph;
+import org.structnetalign.Edge;
 import org.structnetalign.HomologyEdge;
 import org.structnetalign.InteractionEdge;
 import org.structnetalign.util.EdgeWeighter;
 
 import edu.uci.ics.jung.graph.UndirectedGraph;
+import edu.uci.ics.jung.graph.util.Pair;
 
 public class HomologySearchJob implements Callable<InteractionUpdate> {
+
+	private static final Logger logger = LogManager.getLogger("org.structnetalign");
 
 	private CleverGraph graph;
 	private int maxDepth = Integer.MAX_VALUE;
@@ -41,34 +49,31 @@ public class HomologySearchJob implements Callable<InteractionUpdate> {
 	private final int rootA;
 	private final int rootB;
 
-	private EdgeWeighter<HomologyEdge> weighter = new EdgeWeighter<HomologyEdge>() {
-		@Override
-		public double getWeight(HomologyEdge e) {
-			return e.getWeight();
-		}
-	};
-
 	public HomologySearchJob(InteractionEdge root, CleverGraph graph) {
 		super();
 		this.graph = graph;
 		this.root = root;
-		rootA = graph.getInteraction().getSource(root);
-		rootB = graph.getInteraction().getDest(root);
+		Pair<Integer> pair = graph.getInteraction().getEndpoints(root);
+		rootA = pair.getFirst();
+		rootB = pair.getSecond();
 	}
 
 	@Override
 	public InteractionUpdate call() throws Exception {
 		double score = 0;
 		InteractionUpdate update = new InteractionUpdate(root, rootA, rootB);
-		Map<Integer, Double> distancesToA = findDistances(rootA, graph.getHomology(), weighter);
-		Map<Integer, Double> distancesToB = findDistances(rootB, graph.getHomology(), weighter);
+		Map<Integer, Double> distancesToA = findDistances(rootA, graph.getHomology());
+		Map<Integer, Double> distancesToB = findDistances(rootB, graph.getHomology());
 		for (Map.Entry<Integer, Double> a : distancesToA.entrySet()) {
 			for (Map.Entry<Integer, Double> b : distancesToB.entrySet()) {
 				InteractionEdge interaction = graph.getInteraction().findEdge(a.getKey(), b.getKey());
 				if (interaction != null) {
-					double scoreA = 1 - Math.exp(a.getValue());
-					double scoreB = 1 - Math.exp(b.getValue());
-					score += interaction.getWeight() * (1 - scoreA - scoreB + scoreA*scoreB);
+					Double distToA = a.getValue();
+					Double distToB = b.getValue();
+					if (distToA == null || distToB == null) continue; // we can't reach one of a or b
+					double updateProb = interaction.getWeight() * Math.exp(distToA) * Math.exp(distToB);
+					logger.debug("Updating " + root + " (" + rootA + ", " + rootB + ")" + " with score " + nf.format(updateProb) + " due to " + interaction + " (" + a.getKey() + ", " + b.getKey() + ")");
+					score += updateProb - score * updateProb;
 				}
 			}
 		}
@@ -80,20 +85,25 @@ public class HomologySearchJob implements Callable<InteractionUpdate> {
 		this.maxDepth = maxDepth;
 	}
 
-	private <V, E> NavigableMap<V, Double> findDistances(V root, UndirectedGraph<V, E> graph, EdgeWeighter<E> weighter) {
+	private static NumberFormat nf = new DecimalFormat();
+	static {
+		nf.setMinimumFractionDigits(1);
+		nf.setMaximumFractionDigits(3);
+	}
+	
+	private <V, E extends Edge> NavigableMap<V, Double> findDistances(V root, UndirectedGraph<V, E> graph) {
 
 		NavigableMap<V, Double> map = new TreeMap<>();
+		map.put(root, 1.0);
 
 		// need constant-time get
 		HashSet<V> unvisited = new HashSet<V>(graph.getVertices());
 
+		HashMap<V, V> verticesTaken = new HashMap<>();
 		HashMap<V, E> edgesTaken = new HashMap<>();
 		HashMap<V, Integer> distances = new HashMap<>();
+		distances.put(root, 0);
 
-		// first get a root for the cluster
-		Set<V> cluster = new HashSet<V>();
-		unvisited.remove(root);
-		cluster.add(root);
 
 		Deque<V> queue = new LinkedList<V>();
 		queue.add(root);
@@ -102,27 +112,40 @@ public class HomologySearchJob implements Callable<InteractionUpdate> {
 
 			V vertex = queue.remove();
 			E edge = edgesTaken.get(vertex);
+			unvisited.remove(vertex);
 
-			// stop traversing if we're too far
+//			 stop traversing if we're too far
 			int distance = distances.get(vertex);
-			if (distance > maxDepth) continue;
-
+			if (distance > maxDepth) {
+				logger.trace("Distance of " + distance + " exceeded");
+				continue;
+			}
+			
+			double totalWeight = Math.log(1);
+			V parent = verticesTaken.get(vertex);
+			if (parent != null) { // we're not on the root
+				double weight = Math.log(edge.getWeight());
+				totalWeight = map.get(parent) + weight;
+			}
+			logger.trace("Weight for " + vertex + " is " + nf.format(totalWeight));
+			map.put(vertex, totalWeight);
+			
 			Collection<V> neighbors = graph.getNeighbors(vertex);
 			for (V neighbor : neighbors) {
 				if (unvisited.contains(neighbor)) {
 					queue.add(neighbor);
-					unvisited.remove(neighbor);
-					cluster.add(neighbor);
 					E edgeToNeighbor = graph.findEdge(vertex, neighbor);
 					distances.put(neighbor, distance + 1);
-					double score = Math.log(weighter.getWeight(edge));
-					map.put(neighbor, map.get(vertex) + score);
+					verticesTaken.put(neighbor, vertex);
 					edgesTaken.put(neighbor, edgeToNeighbor);
 				}
 			}
 
 		}
 
+		for (Map.Entry<V, Double> entry : map.entrySet()) {
+			logger.trace("Prob(" + root + ", " + entry.getKey() + ") = " + nf.format(Math.exp(entry.getValue())));
+		}
 		return map;
 
 	}
