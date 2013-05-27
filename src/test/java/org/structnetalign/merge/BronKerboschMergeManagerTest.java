0a9ba77063be00ff99b3d416376c9cac5bb8bf07
dmyersturnbull <dmyersturnbull@gmail.com>
2013-05-26 18:03:13 -0700

    Added a merging test case where an interaction edge and a homology edge are both present between the same two vertices.

--- src/test/java/org/structnetalign/merge/BronKerboschMergeManagerTest.java
+++ src/test/java/org/structnetalign/merge/BronKerboschMergeManagerTest.java
@@ -96,6 +96,24 @@ public class BronKerboschMergeManagerTest {
 		assertEquals(6, graph.getInteractionCount()); // just a sanity check for the test itself
 		BronKerboschMergeManager merge = new BronKerboschMergeManager();
 		merge.merge(graph);
+		boolean inter = TestUtils.compareInteractionGraph(graph.getInteraction(), interactionOutput);
+		assertTrue("Interaction graph differs from expected", inter);
+		boolean hom = TestUtils.compareHomologyGraph(graph.getHomology(), homologyOutput);
+		assertTrue("Homology graph differs from expected", hom);
+	}
+	
+	@Test
+	public void testMultiedged() {
+		File homologyInput = new File(RESOURCE_DIR + "multiedge_hom.graphml.xml");
+		File interactionInput = new File(RESOURCE_DIR + "multiedge_int.graphml.xml");
+		File homologyOutput = new File(RESOURCE_DIR + "multiedge_hom_merged.graphml.xml");
+		File interactionOutput = new File(RESOURCE_DIR + "multiedge_int_merged.graphml.xml");
+		CleverGraph graph = GraphMLAdaptor.readGraph(interactionInput, homologyInput);
+		assertEquals(3, graph.getVertexCount()); // just a sanity check for the test itself
+		assertEquals(2, graph.getHomologyCount()); // just a sanity check for the test itself
+		assertEquals(2, graph.getInteractionCount()); // just a sanity check for the test itself
+		BronKerboschMergeManager merge = new BronKerboschMergeManager();
+		merge.merge(graph);
 //		GraphMLAdaptor.writeHomologyGraph(graph.getHomology(), new File("ahom.xml.tmp"));
 //		GraphMLAdaptor.writeInteractionGraph(graph.getInteraction(), new File("aint.xml.tmp"));
 		boolean inter = TestUtils.compareInteractionGraph(graph.getInteraction(), interactionOutput);
